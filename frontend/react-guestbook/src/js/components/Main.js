5b0564c3373e8bd7a721144967844fb130c03a8c
Taneli Peltoniemi <taneli.peltoniemi@gmail.com>
2018-06-11 21:28:46 +0300

    Started working on the frontend

---
+++ frontend/react-guestbook/src/js/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import "../../App.css"
+
+const Main = () => (
+    <div className="App">
+        <header className="App-header">
+            <h1 className="App-title">Guestbook</h1>
+        </header>
+    </div>
+);
+export default Main;
