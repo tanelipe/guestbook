1d0bd2f6fdf4d7fbb9c9a8977a0033afd504aa62
Taneli Peltoniemi <taneli.peltoniemi@gmail.com>
2018-06-11 22:22:25 +0300

    Started working on the UI for the form

--- frontend/react-guestbook/src/js/components/GuestbookForm.js
+++ frontend/react-guestbook/src/js/components/GuestbookForm.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
-
+import { Form, FormGroup, Col, FormControl, ControlLabel, Button } from 'react-bootstrap';
 
 class GuestbookForm extends Component {
     render() {
         return (
-            <div>
-                <p>Hello World</p>
+            <div className="GuestbookForm">
+                <Form horizontal>
+                    <FormGroup controlId="formHorizontalName">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Name
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl type="text" placeholder="Name" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup controlId="formHorizontalText">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Entry
+                        </Col>
+                        <Col sm={10}>  
+                            <FormControl componentClass="textarea" placeholder="Entry" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Button type="submit">SUBMIT</Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
             </div>
         );
     }
--- frontend/react-guestbook/src/js/components/Main.js
+++ frontend/react-guestbook/src/js/components/Main.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import { Row, Col } from 'react-bootstrap'
+
+
 import GuestbookForm from "./GuestbookForm"
 import GuestbookEntries from "./GuestbookEntries"
+
 
 import "../../App.css"
 
@@ -10,8 +14,21 @@ const Main = () => (
         <header className="App-header">
             <h1 className="App-title">Guestbook</h1>
         </header>
-        <GuestbookForm />
-        <GuestbookEntries />
+        <Row>
+            { /* For some reason the mdOffset={4} doesn't work --> can't center */ }
+            <Col md={4}></Col>
+            <Col md={4}>
+                <GuestbookForm />
+            </Col>
+        </Row>
+        <Row>
+            { /* For some reason the mdOffset={4} doesn't work --> can't center */ }
+            <Col md={4}></Col>
+            <Col md={4}>
+                <GuestbookEntries />
+            </Col>
+        </Row>
+
     </div>    
 );
 export default Main;
