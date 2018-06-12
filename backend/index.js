4736de6b60d439308d8990320f32cae1fac0f341
Taneli Peltoniemi <taneli.peltoniemi@gmail.com>
2018-06-12 18:19:40 +0300

    Added CORS handling to the server side

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const helmet = require("helmet")
 const http = require("http")
 const mongoose = require("mongoose")
+const cors = require("cors")
 
 const routes = require("./routes/")
 
@@ -11,6 +12,7 @@ const mongodb_url = process.env.MONGODB_URI || "mongodb://localhost:27017/guestb
 const app = express()
 const router = express.Router()
 const server = http.createServer(app)
+
 
 try {
     mongoose.connect(mongodb_url, {
@@ -22,8 +24,10 @@ try {
 // Setup the routes
 routes(router)
 
+app.use(cors())
 app.use(helmet())
 app.use(bodyParser.json())
 app.use("/api", router)
+
 
 server.listen(8080);
